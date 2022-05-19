e3f7b2df382bbae799206cec9644cf8730cad7c7
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-05-19 08:23:23 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./styles.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import bootstrap from "bootstrap"; // eslint-disable-line no-unused-vars
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "/navbar/navbar.js";
+import Notifications from "/notifications/notifications.js";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="App m-0 p-0">
+      <div className="notification">
+        Trebuie să te înregistrezi/conectezi înainte de a accesa această
+        opțiune!
+      </div>
+      <NavBar />
+      <Notifications />
     </div>
   );
 }
